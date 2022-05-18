65a8cb3334ffc163264d2c42539e525ac6adc6d0
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-18 20:55:36 -0300

    criando os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimentacaoListComponent,
+    MovimentacaoNewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
