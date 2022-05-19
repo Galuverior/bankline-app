c5604163ddb325b32a90ebb47de052551919a6e7
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-19 03:30:49 -0300

    versão final do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LOCALE_ID } from "@angular/core";
 import { registerLocaleData } from "@angular/common";
 import localePt from '@angular/common/locales/pt';
 import { CurrencyMaskModule } from "ng2-currency-mask";
+import { CorrentistaComponent } from './components/correntista/correntista.component';
 
 registerLocaleData(localePt);
 
@@ -18,7 +19,8 @@ registerLocaleData(localePt);
   declarations: [
     AppComponent,
     MovimentacaoListComponent,
-    MovimentacaoNewComponent
+    MovimentacaoNewComponent,
+    CorrentistaComponent
   ],
   imports: [
     BrowserModule,
