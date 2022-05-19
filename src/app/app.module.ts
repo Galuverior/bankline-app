14f6e655347465e12d0db5f8e6209735e7d0da03
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-19 02:51:42 -0300

    criando camada de serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
@@ -8,6 +7,12 @@ import { MovimentacaoNewComponent } from './components/movimentacao-new/moviment
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { LOCALE_ID } from "@angular/core";
+import { registerLocaleData } from "@angular/common";
+import localePt from '@angular/common/locales/pt';
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -20,9 +25,10 @@ import { HttpClientModule } from "@angular/common/http";
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CurrencyMaskModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
